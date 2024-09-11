297e5926d737c51e60a22580a1c9a4c4063677df
RadwanWalid <132624884+Radwan-Walid@users.noreply.github.com>
2024-09-11 19:03:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -26,14 +26,14 @@ var app = express();
 app.use(express.json({limit: '200mb'}));
 app.use(express.urlencoded({limit: '200mb', extended: true }));
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://play-bricks.vercel.app/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://play-bricks.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
 });
 
 app.use(cors({
-  origin: "https://play-bricks.vercel.app/",
+  origin: "https://play-bricks.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
