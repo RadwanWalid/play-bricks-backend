3add457f22a0855e2de741f139503b6712238cb5
RadwanWalid <132624884+Radwan-Walid@users.noreply.github.com>
2024-09-11 18:53:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var app = express();
 app.use(express.json({limit: '200mb'}));
 app.use(express.urlencoded({limit: '200mb', extended: true }));
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://play-bricks.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', "*");
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
