c890494b6f9ab3cb845e4f0d839c117980e945e3
RadwanWalid <132624884+Radwan-Walid@users.noreply.github.com>
2024-09-11 18:40:17 +0300

    Adjustments

--- app.js
+++ app.js
@@ -26,14 +26,14 @@ var app = express();
 app.use(express.json({limit: '200mb'}));
 app.use(express.urlencoded({limit: '200mb', extended: true }));
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://https://play-bricks.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://play-bricks.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
 });
 
 app.use(cors({
-  origin: ["http://https://play-bricks.vercel.app"],
+  origin: ["https://play-bricks.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
@@ -86,5 +86,5 @@ app.use('/Model', ModeRoute);
  * -------------- SERVER ----------------
  */
 
-// Server listens on http://https://play-bricks.vercel.app
+// Server listens on https://play-bricks.vercel.app
 app.listen(5000);
